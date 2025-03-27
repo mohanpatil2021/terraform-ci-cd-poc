04ade6318b20cdc8badd0438b4d10023f05c5a02
mohanpatil2021 <98644471+mohanpatil2021@users.noreply.github.com>
2025-03-27 15:51:53 +0530

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,15 @@
+# variable "backend_config" {
+#     type = object({
+#         resource_group_name = string
+#         storage_account_name = string
+#         container_name       = string
+#         key                  = string
+#     })
+
+#     default = {
+#         resource_group_name = "backendResourceGroup"
+#         storage_account_name = "backendStorageAccount"
+#         container_name       = "backendContainer"
+#         key                  = "backend.tfstate"
+#     }
+# }
